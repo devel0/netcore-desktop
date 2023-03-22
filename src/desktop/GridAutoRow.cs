9025ad20f77edcd0353e583643ca5ac21dfb7b09
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-03-22 10:21:08 +0100

    refactor method name

--- src/desktop/GridAutoRow.cs
+++ src/desktop/GridAutoRow.cs
@@ -1,5 +1,6 @@
 using Avalonia.Data;
 using Avalonia.Interactivity;
+using System.Reactive;
 
 namespace SearchAThing.Desktop;
 
@@ -13,10 +14,11 @@ public class GridAutoRow : AvaloniaObject
 
     static GridAutoRow()
     {
-        AutoRowDefinitionsProperty.Changed.Subscribe(x => HandleAutoRowDefinitionsChanged(x.Sender, x.NewValue.GetValueOrDefault<bool>()));
+        AutoRowDefinitionsProperty.Changed.Subscribe(x =>
+            HandleAutoRowDefinitionsChanged(x.Sender, x.NewValue.GetValueOrDefault<bool>()));
     }
 
-    private static void HandleAutoRowDefinitionsChanged(IAvaloniaObject sender, bool v)
+    private static void HandleAutoRowDefinitionsChanged(AvaloniaObject sender, bool v)
     {
         if (v && sender is Grid grid)
         {
@@ -34,7 +36,7 @@ public class GridAutoRow : AvaloniaObject
 
             foreach (var child in grid.Children.OfType<Control>())
             {
-                if (GridAutoRow.GetName(child) is string childAutoRowName)
+                if (GridAutoRow.GetRef(child) is string childAutoRowName)
                 {
                     if (!namesConfigured.TryGetValue(childAutoRowName, out var configuredRow))
                     {
@@ -52,16 +54,16 @@ public class GridAutoRow : AvaloniaObject
         }
     }
 
-    #region Name ( Attached Property )
+    #region Ref ( Attached Property )
 
-    public static readonly AttachedProperty<string?> NameProperty =
-        AvaloniaProperty.RegisterAttached<GridAutoRow, Interactive, string?>("Name", null, false, BindingMode.OneTime);
+    public static readonly AttachedProperty<string?> RefProperty =
+        AvaloniaProperty.RegisterAttached<GridAutoRow, Interactive, string?>("Ref", null, false, BindingMode.OneTime);
 
-    public static void SetName(AvaloniaObject element, string? parameter) =>
-        element.SetValue(NameProperty, parameter);
+    public static void SetRef(AvaloniaObject element, string? parameter) =>
+        element.SetValue(RefProperty, parameter);
 
-    public static string? GetName(AvaloniaObject element) =>
-        element.GetValue(NameProperty);
+    public static string? GetRef(AvaloniaObject element) =>
+        element.GetValue(RefProperty);
 
     #endregion
 
